528dc13e31489a4154c03eaab107fca7137e78a7
Vishnu Kadaba <vishnuswarupkadaba@gmail.com>
2025-02-13 17:15:04 -0800

    client messaage callbacks

--- vm_sub.py
+++ vm_sub.py
@@ -6,41 +6,62 @@ import paho.mqtt.client as mqtt
 """This function (or "callback") will be executed when this client receives 
 a connection acknowledgement packet response from the server. """
 
+user = "rnene"
+
+
 def on_connect(client, userdata, flags, rc):
     """Once our client has successfully connected, it makes sense to subscribe to
-    all the topics of interest. Also, subscribing in on_connect() means that, 
+    all the topics of interest. Also, subscribing in on_connect() means that,
     if we lose the connection and the library reconnects for us, this callback
     will be called again thus renewing the subscriptions"""
 
-    print("Connected to server (i.e., broker) with result code "+str(rc))
-    #replace user with your USC username in all subscriptions
-    client.subscribe("user/ipinfo")
-    
-    #Add the custom callbacks by indicating the topic and the name of the callback handle
+    print("Connected to server (i.e., broker) with result code " + str(rc))
+    # replace user with your USC username in all subscriptions
+    client.subscribe(f"{user}/ipinfo")
+    client.subscribe(f"{user}/date")
+    client.subscribe(f"{user}")
+
+    # Add the custom callbacks by indicating the topic and the name of the callback handle
     client.message_callback_add("user/ipinfo", on_message_from_ipinfo)
+    client.message_callback_add("user/date", on_message_from_date)
+    client.message_callback_add("user", on_message_from_date)
 
 
 """This object (functions are objects!) serves as the default callback for 
 messages received when another node publishes a message this client is 
 subscribed to. By "default,"" we mean that this callback is called if a custom 
 callback has not been registered using paho-mqtt's message_callback_add()."""
+
+
 def on_message(client, userdata, msg):
-    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+    print(
+        "Default callback - topic: "
+        + msg.topic
+        + "   msg: "
+        + str(msg.payload, "utf-8")
+    )
 
-#Custom message callback.
+
+# Custom message callback.
 def on_message_from_ipinfo(client, userdata, message):
-   print("Custom callback  - IP Message: "+message.payload.decode())
+    print("Custom callback  - IP Message: " + message.payload.decode())
 
 
+def on_message_from_date(client, userdata, message):
+    print("Custom callback  - Date Message: " + message.payload.decode())
 
 
-if __name__ == '__main__':
-    
-    #create a client object
+def on_message_from_user(client, userdata, message):
+    print("Custom callback  - User Message: " + message.payload.decode())
+
+
+if __name__ == "__main__":
+
+    # create a client object
     client = mqtt.Client()
-    #attach a default callback which we defined above for incoming mqtt messages
+    # attach a default callback which we defined above for incoming mqtt messages
     client.on_message = on_message
-    #attach the on_connect() callback function defined above to the mqtt client
+    # attach the on_connect() callback function defined above to the mqtt client
     client.on_connect = on_connect
 
     """Connect using the following hostname, port, and keepalive interval (in 
@@ -50,7 +71,7 @@ if __name__ == '__main__':
     The keepalive interval indicates when to send keepalive packets to the 
     server in the event no messages have been published from or sent to this 
     client. If the connection request is successful, the callback attached to
-    `client.on_connect` will be called."""    
+    `client.on_connect` will be called."""
     client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
 
     """In our prior labs, we did not use multiple threads per se. Instead, we
